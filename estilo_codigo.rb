eac757397a6a09a5dd34bef20eb6574d2a6419ea
codeamac09 <codeamac09@gmail.com>
2016-04-07 19:32:13 -0500

    Completed

---
+++ estilo_codigo.rb
@@ -0,0 +1,55 @@
+class Person
+    @@life_stage = {childhood: 12, teenager: 19, adult: 50}
+    @@legal_age = 18
+    attr_reader :name
+    attr_writer :name
+
+    def initialize (name, age)
+        @name = name
+        @age = age
+    end
+
+    def age
+        @age
+    end
+
+    def life_stage
+        if @age < @@life_stage[:childhood] 
+            :childhood
+        elsif @age < @@life_stage[:teenager]
+            :teenager
+        elsif @age < @@life_stage[:adult]
+            :adult
+        else
+            :elder
+        end
+    end
+
+    def legal
+        @age >= @@legal_age
+    end
+end
+
+
+# Esta parte del código son pruebas. 
+# Antes y después deben de imprimir puros "true"
+fernando = Person.new("Fernando",5)
+juan = Person.new("Juan",45)
+laura = Person.new("Laura",87)
+andrea = Person.new("Andrea",13)
+
+puts fernando.legal == false
+puts juan.legal == true
+puts laura.legal == true
+puts andrea.legal == false
+
+puts fernando.life_stage == :childhood
+puts juan.life_stage == :adult
+puts laura.life_stage == :elder
+puts andrea.life_stage == :teenager
+
+puts laura.age == 87
+laura.name = "Ximena"
+puts laura.name == "Ximena"
+
+
